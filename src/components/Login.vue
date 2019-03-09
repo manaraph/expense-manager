816256f2edfb4aae4a1dfc0027381a228b9ca826
Manasseh Omachonu <manaraph225@gmail.com>
2019-03-09 23:33:35 +0100

    Add expense and login component

---
+++ src/components/Login.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="hello">
+    <div class="wrapper fadeInDown">
+        <div id="formContent">
+            <!-- Tabs Titles -->
+
+            <!-- Icon -->
+            <div class="fadeIn first">
+                <span class="glyphicon glyphicon-user"></span>
+            </div>
+
+            <!-- Login Form -->
+            <form>
+            <input type="text" id="username" class="fadeIn second" name="username" placeholder="username">
+            <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
+            <input type="submit" class="fadeIn fourth" value="Log In">
+            </form>
+
+            <!-- Remind Passowrd -->
+            <div id="formFooter">
+            <a class="underlineHover" href="#">Forgot Password?</a>
+            </div>
+
+        </div>
+        </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'login',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import '../assets/css/style.css';
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
