b3e5be4ead83f507126f68a57a33be31869fbf51
Manasseh Omachonu <manaraph225@gmail.com>
2019-03-09 23:38:38 +0100

    some adjustments

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <login/>
-    <expense/>
+    <!-- <expense/> -->
   </div>
 </template>
 
