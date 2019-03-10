bbd9b7062743a54842b6a6f0cecd33b3009da757
Manasseh Omachonu <manaraph225@gmail.com>
2019-03-10 01:14:06 +0100

    Improvements in UI

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
+    <header>
+      <h3>Please log in to view your expense(s)</h3>
+    </header>
     <login/>
     <!-- <expense/> -->
+    <footer> <p>&copy;{{new Date().getFullYear()}} - manaraph (Omachonu Manasseh). </p></footer>
   </div>
 </template>
 
@@ -19,6 +23,16 @@ export default {
 </script>
 
 <style>
+header{
+  top: 0;
+  left: 10px;
+  margin-top: 20px;
+  position: fixed;
+  width: 100%;  
+}
+h3{
+  /* color: white; */
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -26,5 +40,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+footer{
+  left: 0;
+  bottom: 0;
+  margin: 0;
+  padding: 0;
+  position: fixed;
+  width: 100%;  
 }
 </style>
